86d7eff298373ae841bd11bd07e3ef9738847d3e
richard jackson <denezt@yahoo.com>
2024-02-21 16:04:09 +0100

    Updating, the logo image and py2shell main script

--- py2shell.py
+++ py2shell.py
@@ -18,24 +18,27 @@ from configparser import ConfigParser
 # Set Program Parameters
 parser = argparse.ArgumentParser(
     prog='py2shell', description='Tool for automating shell scripts creation')
-parser.add_argument('--make-executable', action='store_true',
-                    default=False, help='Make script executable')
-parser.add_argument('--datasource', type=str, help='Path to JSON data source')
-parser.add_argument('--output', type=str,
-                    help='Name of target output script name')
-parser.add_argument("--debug", action="store_true", help="Enable debug mode")
-parser.add_argument("--templates", action="store_true",
-                    help="Show available templates")
+parser.add_argument('--datasource','-ds','--source','-s', type=str, help='Path to JSON data source')
+parser.add_argument('--output','-o', type=str, help='Name of target output script name')
+parser.add_argument('--debug','-d','--verbose','-v', action="store_true", help="Enable debug mode")
+parser.add_argument('--make-executable','-me', action='store_true', default=False, help='Make script executable')
+parser.add_argument('--templates','-t', action="store_true", default=False, help="Show available templates")
+# Planned feature to append templates
+# parser.add_argument('--declare', '--funcs', '--stmts', action="append")
+# ./py2shell --declare declarations --funcs functions --stmts statements --output full-script.sh
+# Namespace(n=['declarations', 'functions', 'statements'])
 
 args = parser.parse_args()
 
-# Read the JSON file containing the template data
-datasource = args.datasource
-output = args.output
-view_templates = args.templates
-
 if args.debug:
     print(args)
+
+# Read the JSON file containing the template data
+datasource = args.datasource
+
+output = args.output
+view_templates = args.templates
+make_executable = args.make_executable
 
 if '__main__' == __name__:
     if output is not None and datasource is not None:
@@ -57,16 +60,21 @@ if '__main__' == __name__:
                     script_name = "scripts/{}".format(output)
                     # Automatically make scripts executeable
                     if os.path.isfile(script_name):
-                        print("Making, script executeable")
-                        try:
-                            os.chmod("{}".format(script_name), 0o777)
-                        except FileNotFoundError as fnfe:
-                            print(fnfe)
-                    print("Template generated successfully!")
+                        if make_executable:
+                            print(f"\033[35mMaking, scripts \033[36m{script_name} \033[35mexecutable.\033[0m")
+                            try:
+                                os.chmod("{}".format(script_name), 0o777)
+                            except FileNotFoundError as fnfe:
+                                print(fnfe)
+                        else:
+                            print(f"\033[35mCreated, non-executable script \033[36m{script_name}\033[0m")
+                    print("\033[32mTemplate generated successfully!\033[0m")
             else:
                 print("Error: Output script name must end with an '.sh' extension")
         except AttributeError as ae:
             pass
-    if view_templates:
+    elif view_templates:
         t = TemplateLister.TemplateLister()
         t.createMenu()
+    else:
+        print("Missing target output name or datasource template")
